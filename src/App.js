ec29d66a75dcc96d1d0584f2623dee59693b7e55
joes <josephgavinsyverson@gmail.com>
2019-12-15 03:20:45 -0500

    Set API data to state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 
+
 function Map() {
 	return (
 		<GoogleMap
@@ -13,18 +14,35 @@ function Map() {
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 class App extends React.Component {
-  render() {
-    return (
-      <div style={{width: '100vw', height: "100vh"}}>
-      	<WrappedMap
-      	googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAKXQV1Q3X6HGxKS85QA0HWMz6024_1b8g"}
-      	loadingElement={<div style={{height: "100%"}}/>}
-      	containerElement={<div style={{height: "100%"}}/>}
-      	mapElement={<div style={{height: "100%"}}/>}
-      	/>
-      </div>
-    );
-  }
+	state = {breweries: []}
+
+	// ----- MAP CONFIG ----- \\
+	
+	// ----- FETCHES ----- \\
+
+	fetchData = () => {
+    	fetch('https://api.openbrewerydb.org/breweries?by_city=las_vegas')
+    	.then((res) => res.json()).then((json) => this.setState({breweries: json}));
+  	}
+
+	// ----- LIFECYCLE ----- \\
+
+	componentDidMount() {
+		this.fetchData()
+	}
+
+	render() {
+	return (
+	  <div style={{width: '100vw', height: "100vh"}}>
+	  	<WrappedMap
+	  	googleMapURL={"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAKXQV1Q3X6HGxKS85QA0HWMz6024_1b8g"}
+	  	loadingElement={<div style={{height: "100%"}}/>}
+	  	containerElement={<div style={{height: "100%"}}/>}
+	  	mapElement={<div style={{height: "100%"}}/>}
+	  	/>
+	  </div>
+	);
+	}
 }
 
 // "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
